1c231cb9f2e2caef1025810a5e60bc613f524db4
keenager <keenager84@gmail.com>
2022-03-01 02:12:19 +0900

    css 반영

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
+import styles from './Movie.module.css';
 
-function Movie({id, coverImg, title, genres, summary }) {
+function Movie({ id, coverImg, title, genres, summary }) {
     return (
-        <div>
-            <img src={coverImg} alt={title} />
-            <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-            <ul>
-                {genres.map((genre) => (
-                    <li key={genre}>{genre}</li>
-                ))}
-            </ul>
-            <p>{summary}</p>
+        <div className={styles.movie}>
+            <img src={coverImg} alt={title} className={styles.movie__img} />
+            <div>
+                <h2 className={styles.movie__title}>
+                    <Link to={`/movie/${id}`}>{title}</Link>
+                </h2>
+                <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                <ul className={styles.movie__genres}>
+                    {genres.map((genre) => (
+                        <li key={genre}>{genre}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
